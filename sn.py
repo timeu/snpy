66728372e7622dbf340a32635aae92e18a1d32c6
Uemit Seren <uemit.seren@gmail.com>
2016-04-13 17:48:36 +0200

    Enhancement: Add support for Ancestry
    Change the various handle methods to accept a file descriptor instead of a filename and defer the opening to the parse method.
    Additionally add method guess_source_from_file and guess_source_from_content to guess the source from the contents instead of the filename

--- sn.py
+++ sn.py
@@ -49,8 +49,8 @@ class SNP(_SNP):
                                        int(position), strand, genotype)
 
 
-def _23andme(path):
-    handle = csv.DictReader(open(path, "r"),
+def _23andme(fd):
+    handle = csv.DictReader(fd,
         fieldnames=["name", "chromosome", "position", "genotype"],
         delimiter="\t")
 
@@ -59,11 +59,11 @@ def _23andme(path):
             yield SNP(**row)
 
 
-def _23andme_exome(path):
+def _23andme_exome(fd):
     if vcf is None:
         raise RuntimeError("PyVCF not available, please 'easy_install' it.")
 
-    for r in vcf.VCFReader(open(path, "r")):
+    for r in vcf.VCFReader(fd):
         if not r.is_snp:
             continue  # XXX Is it even possible?
 
@@ -72,8 +72,8 @@ def _23andme_exome(path):
                       genotype=sample.gt_bases.replace("/", ""))
 
 
-def decodeme(path):
-    handle = csv.DictReader(open(path, "r"),
+def decodeme(fd):
+    handle = csv.DictReader(fd,
         fieldnames=["name", "variation", "chromosome", "position",
                     "strand", "genotype"])
 
@@ -84,15 +84,25 @@ def decodeme(path):
             yield SNP(**row)
 
 
-def ftdna(path):
-    handle = csv.DictReader(open(path, "r"),
+def ftdna(fd):
+    handle = csv.DictReader(fd,
         fieldnames=["name", "chromosome", "position", "genotype"])
 
     for row in handle:
         if row["position"].isdigit():
             yield SNP(**row)
 
-
+def ancestry(fd):
+    handle = csv.DictReader(fd,
+        fieldnames=["name", "chromosome", "position", "allele1","allele2"],
+        delimiter="\t")
+    for row in handle:
+        if not row["name"].startswith("#") and row["position"].isdigit():
+            row['genotype'] = row['allele1']+row['allele2']
+            del(row['allele1'])
+            del(row['allele2'])
+            yield SNP(**row)
+    
 def guess_source(path):
     name, ext = os.path.split(path)
     if ext == "vcf":
@@ -105,6 +115,44 @@ def guess_source(path):
         raise UnknownSource(path)
     else:
         return source
+
+
+def _guess_source_from_generator(it):
+    first_line = next(it)
+    # check if 23andMe or ancestry
+    if first_line[0] == '#':
+        if 'AncestryDNA' in first_line:
+            return 'ancestry'
+        elif '23andMe' in first_line:
+            return '23andme'
+        # if can't match first line iterate until header line and check there
+        for row in it:
+            if row.startswith("#"): 
+                continue
+            fields = row.split('\t')
+            if len(fields) == 5:
+                return 'ancestry'
+            else:
+                return '23andme'
+    else:
+        fields = first_line.split(',')
+        if len(fields) == 6 and fields == ['Name','Variation','Chromosome','Position','Strand','YourCode']:
+            return 'decodeme'
+        elif len(fields) == 5 and fields == ['RSID','CHROMOSOME','POSITION','RESULT']:
+            return 'ftdna'
+    raise UnknownSource(path)
+   
+    
+
+def guess_source_from_file(path):
+    if not hasattr(path, 'read'):
+        with open(path,'r') as fd:
+            return  _guess_source_from_generator(fd)
+    else: 
+        return _guess_source_from_generator(path)
+
+def guess_source_from_content(content):
+    return _guess_source_from_generator(iter(content.splitlines()))
 
 
 def parse(path, source=None):
@@ -120,15 +168,22 @@ def parse(path, source=None):
     """
     if source is None:
         source = guess_source(path)
-
+        
     try:
         handler = {"23andme": _23andme,
                    "23andme-exome-vcf": _23andme_exome,
                    "ftdna-illumina": ftdna,
                    "decodeme": decodeme,
                    "vcf": _23andme_exome,
-                   "ftdna": ftdna}[source]
+                   "ftdna": ftdna,
+                   "ancestry":ancestry}[source]
     except KeyError:
         raise UnknownSource(path)
     else:
-        return handler(path)
+        if not hasattr(path, 'read'):
+            with open(path,'r') as fd:
+                return handler(fd)
+        else: 
+            return handler(path)
+     
+        
